6f180494a3dda45ae720ae3b0be814cab26a5d05
Alexandre Lamarre <alex7285@gmail.com>
2024-03-17 12:28:05 -0400

    [skip ci][wip] rebase onto main
    Signed-off-by: Alexandre Lamarre <alex7285@gmail.com>

--- pkg/lock/backend/embedded/lock_manager.go
+++ pkg/lock/backend/embedded/lock_manager.go
@@ -1,6 +1,10 @@
 package embedded
 
-import "github.com/alexandreLamarre/dlock/pkg/lock"
+import (
+	"context"
+
+	"github.com/alexandreLamarre/dlock/pkg/lock"
+)
 
 type LockManager struct {
 }
@@ -9,6 +13,10 @@ var _ lock.LockManager = (*LockManager)(nil)
 
 func NewLockManager() *LockManager {
 	return &LockManager{}
+}
+
+func (lm *LockManager) Health(ctx context.Context) ([]string, error) {
+	panic("implement me")
 }
 
 func (lm *LockManager) NewLock(key string, opts ...lock.LockOption) lock.Lock {
